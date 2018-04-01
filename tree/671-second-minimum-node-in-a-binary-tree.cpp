2c94ba42d332dcf32a626ce5790b8a8ef5bd6b74
nhannguyen95 <nhannguyenmath95@gmail.com>
2018-04-01 23:03:39 +0200

    add solutions

--- tree/671-second-minimum-node-in-a-binary-tree.cpp
+++ tree/671-second-minimum-node-in-a-binary-tree.cpp
@@ -1,9 +1,12 @@
 // Solution 1: recursion
 // N = number of nodes
 // pattern: post-order
-// O(N) in time (each node is visited once)
-// O(N) in space in worst case (skewed tree)
-//
+// O(N) in time (when the second minimum node
+// is in the right most of the tree, then
+// need to visit all the node to find it)
+// O(N) in space in worst case ("skewed" tree, of
+// course each node still has 2 sub nodes)
+// O(h) in average, h = height of tree.
 class Solution {
 public:
     int findSecondMinimumValue(TreeNode* root) {
@@ -16,9 +19,33 @@ public:
 
         int geLeft = helper(node->left, smallest);
         int geRight = helper(node->right, smallest);
-        int mn = min(geLeft, geRight);
-        int mx = max(geLeft, geRight);
-        if (mn > smallest) return mn;
-        return (mx <= smallest) ? -1 : mx;
+        if (geLeft == -1) return geRight;
+        if (geRight == -1) return geLeft;
+        return min(geLeft, geRight);
+    }
+};
+
+// Solution 2: iterative solution of solution 1
+// Please refer to Python file.
+
+// Solution 3: another iterative solution
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        if (root == NULL) return -1;
+        int min_ = root->val;
+        stack<TreeNode*> stack({root});
+        int res = INT_MAX;
+        while(!stack.empty()) {
+            TreeNode* node = stack.top();
+            stack.pop();
+            if (min_ < node->val && node->val < res)
+                res = node->val;
+            if (node->left != NULL) {
+                stack.push(node->left);
+                stack.push(node->right);
+            }
+        }
+        return (res == INT_MAX) ? -1 : res;
     }
 };
