f1e7e18097351bd26ee0e6be7a21669148bb5f57
Nhan Nguyen Thanh <nhannguyenmath95@gmail.com>
2018-04-20 14:37:22 +0200

    Update 451-sort-characters-by-frequency.cpp

--- heap/451-sort-characters-by-frequency.cpp
+++ heap/451-sort-characters-by-frequency.cpp
@@ -24,3 +24,60 @@ public:
         return res;
     }
 };
+
+// Solution 2: counting sort
+// O(4N) = O(N) in time
+// O(4N) = O(N) in space
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> M1;
+        for(char c : s) M1[c]++;
+        
+        unordered_map<int, vector<char>> M2;
+        vector<int> vals;
+        for(auto p : M1) {
+        	M2[p.second].push_back(p.first);
+        	vals.push_back(p.second);
+        }
+
+        // Counting sort
+        int mx = vals.size() ? *max_element(vals.begin(), vals.end()) : 0;
+        vector<int> buckets(mx+1, 0);
+        for(int val : vals) buckets[val]++;
+
+        // Result
+        string res = "";
+        for(int i = mx; i >= 1; i--) {
+        	if (buckets[i] > 0) {
+        		for(char c : M2[i])
+        			res += string(i, c);
+        	}
+        }
+        return res;
+    }
+};
+
+// Solution 3: couting sort v2
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        for(char c : s) freq[c]++;
+        
+        // Counting sort
+        vector<string> buckets(s.size()+1, "");
+        for(auto& p : freq) {
+            buckets[p.second].append(p.second, p.first);
+        }
+        
+        // Res
+        string res = "";
+        for(int i = (int)s.size(); i >= 1; i--) {
+            if (buckets[i] != "") {
+                res.append(buckets[i]);
+            }
+        }
+        return res;
+    }
+};
