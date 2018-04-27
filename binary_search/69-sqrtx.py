36623909bdc6780429f694781cb030cbbe1927d9
Nhan Nguyen Thanh <nhannguyenmath95@gmail.com>
2018-04-27 21:42:13 +0200

    Create 69-sqrtx.py

---
+++ binary_search/69-sqrtx.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x):
+        # Find maximum y so that y*y <= x
+        low = 0
+        high = x
+        while low < high:
+            mid = low + (high - low + 1) // 2
+            # mid = (low + high + 1) // 2
+            if mid*mid <= x: low = mid
+            else: high = mid - 1
+        return low
