b2a66e6da11af40dd11b31d77942390bc9c1038f
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-08-09 21:42:05 +0700

    Create 717-1-bit-and-2-bit-characters.cpp

---
+++ array/717-1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,21 @@
+// Solution 1: dynamic programming
+// Can use memoization technique to improve
+class Solution {
+public:
+    bool endsWith(vector<int>& bits, int i, int c) {
+        // Some base cases
+        if (i < 0) return true;
+        if (i < 1 && c == 1) return false;
+            
+        if (c == 0)
+            return (bits[i] == 0) &&
+                   (endsWith(bits, i-1, c) || endsWith(bits, i-1, 1-c));   
+        
+        return (bits[i-1] == 1) &&
+               (endsWith(bits, i-2, c) || endsWith(bits, i-2, 1-c));
+    }
+    
+    bool isOneBitCharacter(vector<int>& bits) {
+        return endsWith(bits, bits.size()-1, 0);
+    }
+};
