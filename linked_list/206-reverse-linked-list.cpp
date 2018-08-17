e78e4ce9b6cdbc2009e5b9d9a2c68270756cb778
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-08-17 22:02:53 +0700

    Create 206-reverse-linked-list.cpp

---
+++ linked_list/206-reverse-linked-list.cpp
@@ -0,0 +1,52 @@
+// Solution 1: iterative
+// O(1) in space, O(N) in time
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* head2 = NULL;
+        while(head != NULL) {
+            ListNode* next = head->next;
+            head->next = head2;
+            head2 = head;
+            head = next;
+        }
+        return head2;
+    }
+};
+
+// Solution 2: recursive
+// O(N) in space, O(N) in time
+class Solution {
+public:
+    pair<ListNode*, ListNode*> reverseListHelper(ListNode* node) {
+        if (node == NULL || node->next == NULL)
+            return make_pair(node, node);
+        
+        pair<ListNode*, ListNode*> p = reverseListHelper(node->next);
+        ListNode* head = p.first;
+        ListNode* tail = p.second;
+        
+        tail->next = node;
+        node->next = NULL;
+        
+        return make_pair(head, node);
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        return reverseListHelper(head).first;
+    }
+};
+
+// Solution 3: a more subtle recursive
+// O(N) in time and space
+class Solution {
+public:
+    ListNode* reverseList(ListNode* node) {
+        if (node == NULL || node->next == NULL)
+            return node;
+        ListNode* head = reverseList(node->next);
+        node->next->next = node;
+        node->next = NULL;
+        return head;
+    }
+};
