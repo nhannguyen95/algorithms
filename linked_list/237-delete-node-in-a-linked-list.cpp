3b170a002b6a6341087688aca0571c42585c597d
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-08-17 22:14:07 +0700

    Update 237-delete-node-in-a-linked-list.cpp

--- linked_list/237-delete-node-in-a-linked-list.cpp
+++ linked_list/237-delete-node-in-a-linked-list.cpp
@@ -2,7 +2,9 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        // ListNode* nxt = node->next;
         node->val = node->next->val;
         node->next = node->next->next;
+        // delete nxt;
     }
 };
