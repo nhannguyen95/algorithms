e1b7b696958bcfd90ab838c4f0068e05ea8d9bbb
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-08-25 18:06:22 +0700

    Update 283-move-zeros.cpp

--- array/283-move-zeros.cpp
+++ array/283-move-zeros.cpp
@@ -1,12 +1,17 @@
-// Solution 1:
+// 283 Move Zeros
+// https://leetcode.com/problems/move-zeroes/description/
+// Dutch National Flag algorithm
+
+// Solution 1: for style
 // O(1) in space, O(N) in time
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int slow = 0;
-        for(int fast = 0; fast < nums.size(); fast++) {
-            // Same problem: move non-zero elements to the head.
-            if (nums[fast] != 0) swap(nums[slow++], nums[fast]);
+        int lo = 0;
+        for(int hi = 0; hi < nums.size(); hi++) {
+            // nums[0..lo] = {non-0}
+            // nums[lo+1..hi] = {0}
+            if (nums[hi] != 0) swap(nums[lo], nums[hi--]);
         }
     }
 };
