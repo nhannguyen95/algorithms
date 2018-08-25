d08b1f5ed9ed05a3adff129cedcd4fe95cd337c7
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-08-25 20:44:45 +0700

    Create split-string.cpp

---
+++ handy-algorithms/split-string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+vector<string> splitAll(const string & s, const string & delim) {
+	vector<string> tokens;
+	size_t last = 0;
+	for(size_t next = 0; (next = s.find(delim, last)) != s.npos; ) {
+		tokens.push_back(s.substr(last, next - last));
+		last = next + delim.size();
+	}
+	tokens.push_back(s.substr(last));
+	return tokens;
+}
+
+int main() {
+  string s = "ab cd efg gh";
+  vector<string> tokens = splitAll(s, " ");
+  for(string token :tokens)
+  	cout << "'" << token << "'" << '\n';
+
+  return 0;
+}
