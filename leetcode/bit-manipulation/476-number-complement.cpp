961508f56813dfa7910f80ba48ab9a83a65d50ea
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-09-04 22:42:18 +0700

    Update 476-number-complement.cpp

--- leetcode/bit-manipulation/476-number-complement.cpp
+++ leetcode/bit-manipulation/476-number-complement.cpp
@@ -26,3 +26,19 @@ public:
         return ~num & ~mask;
     }
 };
+
+// Solution 3: Most optimal one
+// n = 0* 1 ?* => mask = 0* 1 1*
+// result = n ^ mask = ~n & mask
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = num;
+        mask |= (mask >> 1);   // 2
+        mask |= (mask >> 2);   // 4
+        mask |= (mask >> 4);   // 8
+        mask |= (mask >> 8);   // 16
+        mask |= (mask >> 16);  // 32
+        return num ^ mask;  // or ~num & mask
+    }
+};
