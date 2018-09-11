c082500f581bee6390c606460229065e31daf1d4
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-09-11 22:34:52 +0700

    Update insertion-sort.cpp

--- what-to-prepare/algorithms/insertion-sort.cpp
+++ what-to-prepare/algorithms/insertion-sort.cpp
@@ -1,12 +1,32 @@
-/* Insertion sort (tested) */
+/* Insertion sort (tested) 
+
+Time complexity:
+  - Best case: the array is already sorted, O(N)
+  - Average: O(N^2)
+  - Worst case: the array sorted in reverse order, O(N^2)
+  
+Space complexity: O(1)
+
+Stable: keep the relative position of elements with equal keys.
+
+Insertion sort is one of the fastest for sorting small arrays (N = 7..50), why?
+  - Compare to Merge sort, Quick sort: they have some extra overhead,
+    (while insertion sort is very simple):
+    - Recursive calls
+    - Partition: need to split N to two (N/2), and so on...
+  - Compare to Selection sort, Bubble sort: probably faster because fewer scans,
+    iterations..
+    
+
+Read more: https://nhannguyen95.github.io/insertion-sort/
+
+*/
 
 // ----------------INCLUDE-----------------
 
 #include <iostream>
 #include <vector>
 using namespace std;
-
-// ----------------------------------------
 
 // -------------INSERTION SORT-------------
 
