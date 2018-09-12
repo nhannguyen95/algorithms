43cb098746212ce5b6c59aac6920eacbf7c28fc9
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-09-12 21:09:01 +0700

    Update merge-sort.cpp

--- what-to-prepare/algorithms/merge-sort.cpp
+++ what-to-prepare/algorithms/merge-sort.cpp
@@ -1,4 +1,32 @@
-/* Merge sort (tested) */
+/* Merge sort (tested) 
+
+Time complexity and Space complexity: read more
+(Recurrence Equation: công thức truy hồi)
+
+  - O(NlogN) time (in worst case)
+  T(n) = C (if right == left => n = 1)
+        = 2 T(n/2)
+   T(n) = 2 T(n/2) + n = 2(2 T(n/4) + n/2) + n = 2^2 T (n / 2^2) + 2n
+        = 2^2 (2 T(n / 2^3) + n / 2^2 ) + 2n
+        = 2^3 T(n / 2^3) + 3n  ...
+        = 2^logn T(n / 2^logn) + nlogn
+        = Cn + nlogn
+   => O(N) = NlogN in time
+  
+   - O(N) space:
+   S(n) = 1 (if right == left => n = 1)
+   S(n) = S(n/2) + n = S(n/2^2) + n + n/2 = S(n/2^3) + n + n/2 + n/2^2 ..
+        = S(n/2^logn) + n + n/2 + .. + n/2^(logn - 1)
+        = n + n/2 + .. + n/2^(logn - 1) + n/2^logn
+        = n(1 + 1/2 + .. + 1/2^logn) -> 2n
+   => O(N) = N in space (not in-place)
+
+Stable
+
+
+Read more: https://nhannguyen95.github.io/merge-sort/
+
+*/
 
 // ----------------INCLUDE-----------------
 
@@ -13,7 +41,7 @@ void merge(vector<int>& arr, int l, int m, int r) {
   vector<int> tmp;
   int i = l, j = m + 1;
   while(i <= m && j <= r) {
-    if (arr[i] < arr[j]) tmp.push_back(arr[i++]);
+    if (arr[i] <= arr[j]) tmp.push_back(arr[i++]);
     else tmp.push_back(arr[j++]);
   }
 
