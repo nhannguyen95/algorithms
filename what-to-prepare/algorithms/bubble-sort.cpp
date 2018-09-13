8215b87d97e99c5df0d14a94a68fe734eb974195
Nhan Nguyen <nhannguyenmath95@gmail.com>
2018-09-13 21:45:24 +0700

    Update bubble-sort.cpp

--- what-to-prepare/algorithms/bubble-sort.cpp
+++ what-to-prepare/algorithms/bubble-sort.cpp
@@ -30,20 +30,39 @@ using namespace std;
 // ---------------BUBBLE SORT--------------
 
 void bubbleSort(vector<int>& arr) {
-  int n = (int)arr.size();
-  for(int k = 1; k < n; k++) {
-    // Now we bring the k-th largest element
-    // to the end of the array (i.e. to the
-    // (n-k) index)
+  // arr[0..n): unsorted
+  for(int n = (int)arr.size(); n > 0; n--) {
+    // Bring largest element in arr[0, n) to (n-1) position
     bool hasSwap = false;
-    for(int i = 0; i < n-k; i++)
-      if (arr[i] > arr[i+1]) {
-        swap(arr[i], arr[i+1]);  // bubble up
+    for(int i = 1; i < n; i++)
+      if (arr[i-1] > arr[i]) {
+        swap(arr[i-1], arr[i]);
         hasSwap = true;
       }
-    
-    // Break if there's no more swap
-    if (!hasSwap) return;
+
+    // Break if there's no swap
+    if (!hasSwap) break;
+  }
+}
+
+// ----------OPTIMIZED BUBBLE SORT---------
+// Idea: At k-th iteration, there are not neccessarily
+// only k (largest) elements placed at their correct position
+// (at the end of the array), but maybe more, maybe we have 
+// k' elements at their corrected position.
+
+// k' is at the last swap at current iteration.
+
+void optimizedBubbleSort(vector<int>& arr) {
+  // arr[0, n): unsorted
+  for(int n = (int)arr.size(); n > 0; ) {
+    int newn = 0;
+    for(int i = 1; i < n; i++)
+      if (arr[i-1] > arr[i]) {
+        swap(arr[i-1], arr[i]);
+        newn = i;
+      }
+    n = newn;
   }
 }
 
