b38c57958714955fef4f3f3b33f7d5badede89bf
Nhan Nguyen <nhannguyenmath95@gmail.com>
2019-01-17 22:52:19 +0700

    Update bridges-articulation-points.cpp

--- algorithms-and-data-structures/algorithms/bridges-articulation-points.cpp
+++ algorithms-and-data-structures/algorithms/bridges-articulation-points.cpp
@@ -12,10 +12,25 @@ Line 28, why isn’t it low[u] = min(low[u], low[v])
 Answer: G is undirected graph (there is no forward and cross edge), so line 28 happen
 only when (u, v) is a back edge.
 
-It means that we visit u when we haven’t finished traversing the DFS tree of v yet,
-meaning that low[v] hasn’t fully computed (low[v] is fully computed only when we finish
-traverse DFS tree of v) => can’t use low[v] here, should be low[u] = min(low[u], num[v])
+Consider the back edge C, A as follows:
 
+   |
+   |
+ _ A (num=4; low=1)
+|  |
+|  |   
+|  B (num=5; low=?)
+|  |
+|  |
+ --C (num=6; low=4, not 1 like in Tarjan)
+ 
+In this problem, you will only want to minimize C's low with the num of the other
+vertice (in this case A) of its back edge (think about it). In other words, the
+low array here is defined with a slight difference with the one used in Tarjan problem:
+- In Tarjan: low[u] = the lowest visit_time of a vertex to which u can reach (since
+we don't care how u (directly/indirectly) reaches that vertex, as long as u can reach it).
+- In this: low[u] = the lowest visit_time of a vertex to which u can reach DIRECTLY
+via its back edges.
 
 */
 
