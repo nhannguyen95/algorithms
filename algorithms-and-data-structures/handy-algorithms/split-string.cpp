2de90ceb1bf10dcd103fba4b78f11ea64f5aa53c
Nhan Nguyen <nhannguyenmath95@gmail.com>
2019-01-28 12:57:21 +0700

    Update split-string.cpp

--- algorithms-and-data-structures/handy-algorithms/split-string.cpp
+++ algorithms-and-data-structures/handy-algorithms/split-string.cpp
@@ -7,15 +7,16 @@ vector<string> splitAll(const string & s, const string & delim) {
 	vector<string> tokens;
 	size_t last = 0;
 	for(size_t next = 0; (next = s.find(delim, last)) != s.npos; ) {
-		tokens.push_back(s.substr(last, next - last));
+    if (last < next)
+      tokens.push_back(s.substr(last, next - last));
 		last = next + delim.size();
 	}
-	tokens.push_back(s.substr(last));
+	if (last < s.size()) tokens.push_back(s.substr(last));
 	return tokens;
 }
 
 int main() {
-  string s = "ab cd efg gh";
+  string s = "  ab cd efg gh   ";
   vector<string> tokens = splitAll(s, " ");
   for(string token :tokens)
   	cout << "'" << token << "'" << '\n';
