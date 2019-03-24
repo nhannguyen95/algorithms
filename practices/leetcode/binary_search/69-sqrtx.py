fea4986e85de7f16546fa1a0a74fa03b86925d3e
Nhan Nguyen <nhannguyenmath95@gmail.com>
2019-03-24 07:39:13 +0700

    Update 69-sqrtx.py

--- practices/leetcode/binary_search/69-sqrtx.py
+++ practices/leetcode/binary_search/69-sqrtx.py
@@ -5,7 +5,7 @@ class Solution:
         high = x
         while low < high:
             mid = low + (high - low + 1) // 2
-            # mid = (low + high + 1) // 2
+            # mid = (low + high + 1) // 2 = low + (high - low) // 2 + (high - low % 2)
             if mid*mid <= x: low = mid
             else: high = mid - 1
         return low
