9c6f15b2e98c96d069b321e0a35912d564a4bad0
Nhan Nguyen <nhannguyenmath95@gmail.com>
2019-05-19 11:04:26 +0700

    Update euler-tour.cpp

--- algorithms-and-data-structures/algorithms/euler-tour.cpp
+++ algorithms-and-data-structures/algorithms/euler-tour.cpp
@@ -1,10 +1,20 @@
 /* Find Euler Tour in a directed graph
 
-An Euler tour of a strongly connected, directed graph G = (V, E)
-is a cycle that traverses each edge of G exactly once (a vertex
-may be visited more than once)
+An Euler path visits each edge of the graph exactly once.
 
-G has an Euler tour if and only if in-degree(v) = out-degree(v)
+An Euler tour of a strongly connected, directed/undirected
+graph G = (V, E) is a CYCLE that traverses each edge of G
+exactly once (a vertex may be visited more than once).
+
+A graph which has either an Euler path or an Euler tour is caled
+an Eulerian graph.
+
+If G is UNDIRECTED
+- G has Euler tour if all vertices have even degrees.
+- G has Euler path if all except for 2 vertices have even degrees.
+
+If G is DIRECTED
+- G has an Euler tour if and only if in-degree(v) = out-degree(v)
 for each vertex v in V
 Proof: Without loss of generality, we can assume that there is
 some vertex v such that in-degree(v) - out-degree(v) > 0, and v
@@ -14,6 +24,8 @@ we necessarily visit v some times. After passing through v
 there are still some in-coming edges we need to use. However we
 cannot use these edges since when we visit v one more time, we
 can never escape from it to go back to the starting point of the cycle.
+- G has Euler path if in-degree(v) = out-degree(v), at most
+one vertex with in - out = 1 and at most one vertex with out - in = 1.
 
 To find Euler tour: we can just arbitrarily walk any way that
 we want so long as we don’t repeat an edge, we will necessarily
