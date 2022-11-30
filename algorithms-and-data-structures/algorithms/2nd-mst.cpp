bc275e8d0e6c01f94a5b00d3adf4066677d749c2
Nhan Nguyen <nhannguyenmath95@gmail.com>
2022-11-30 16:22:36 +0900

    Update 2nd-mst.cpp

--- algorithms-and-data-structures/algorithms/2nd-mst.cpp
+++ algorithms-and-data-structures/algorithms/2nd-mst.cpp
@@ -1,6 +1,6 @@
 /* Finding second best spanning tree in O(VE), the graph is represented in edge list
 
-Observation: MST and MST2 only differ in 2 edges, we take away 1 edge in MST and edge
+Observation: MST and MST2 only differ in 2 edges, we take away 1 edge in MST and add
 another edge to it to form MST2.
 
 Algorithm: first find the MST like normal, then for each edge in the MST (V-1 edge),
