5a837d05054cb5a9f25b1fffcc9263e29df3636f
nhannguyen95 <nhannguyenmath95@gmail.com>
2024-01-05 08:43:56 +0900

    Update Fenwick Tree note

--- algorithms-and-data-structures/data-structures/fenwick-tree.cpp
+++ algorithms-and-data-structures/data-structures/fenwick-tree.cpp
@@ -4,7 +4,7 @@ Fenwick Tree is a useful data structure for implementing
 dynamic cumulative frequency tables.
 
 First we need to understand what (x & (-x)) mean, since
-negative numbers in C++ are represented by 2's complement:
+negative numbers in C++/Java are represented by 2's complement:
 x & (-x) = x & (~x + 1)
 For example:
         x = 1011010100
@@ -17,26 +17,29 @@ We denote x & (-x) = LSOne(x)
 
 Consider the Range Sum Query problem: you are given an array
 A of size n, 2 kinds of query:
-- update i, v: update A[i] = v
-- query l, r: return sum(A[l..r])
+- point_update(i, v): update A[i] += v (v can be positive or negative)
+- range_query(l, r): return sum(A[l..r])
+
+The default Fenwick Tree that supports these 2 operations is
+widely known as Point Update and Range (Sum) Query (PURQ) Fenwick Tree.
 
 The Fenwick Tree will have an array ft of [1..n] nodes,
 the node at index i-th is responsible for elements in the range
 of [i-LSOne(i)+1 .. i], meaning that in this problem:
 ft[i] = sum(A[i-LSOne(i)+1..i])
 
-With this arrangement, we can obtain the cumulative sum of A[1..b]
-by adding:
+With this arrangement, we can calculate range_query(l, r) from
+range_query(b) which is the cumulative sum of A[1..b] by adding:
   ft[b]
 + ft[b'  = b - LSOne(b)]
 + ft[b'' = b' - LSOne(b')]
-(.. until the index != 0)
+(.. until the index = 0)
 (Easily see this runs in O(logN))
 
 Having sum of A[1..b], we can query sum of A[l..r]
 
-How about update A[b] = v? We need to update the nodes that are
-responsible for the range that contains b too, starting from b-th:
+How about update A[b] += v? We need to update the nodes that are
+responsible for the ranges that contain b too, starting from b-th:
 update ft[b]
 update ft[b'  = b + LSOne(b)]
 update ft[b'' = b' + LSOne(b')]
@@ -46,11 +49,37 @@ update ft[b'' = b' + LSOne(b')]
 In summary, Fenwick Tree takes:
 - O(N) in space
 - O(logN) for update, query 
+- O(NlogN) for build
 
 Besides summation, BIT can also be used to support other types of
 operations, e.g. range minimum query, while maintaining the same
 time complexity of O(logN):
 https://ioinformatics.org/journal/v9_2015_39_44.pdf
+
+------------------------------------------------------------------
+
+Range Update Point Query (RUPQ) Fenwick Tree:
+- range_update(l, r, v): update A[l..r] += v (v can be
+positive/negative) in O(logN).
+- point_query(i): return A[i] in O(logN).
+
+For the range update operation, the idea is to perform 2 updates:
+- update(l, v): makes all indices in [l..n] have +v value.
+- update(r+1, -v): makes all indices in [r+1..n] have -v value,
+canceling previous update.
+
+For the Point Query operation, now if we query range_query(i), we
+will correctly get A[i].
+
+------------------------------------------------------------------
+
+Range Update Range Query (RURQ) Fenwick Tree:
+- range_update(l, r, v): update A[l..r] += v(v can be
+positive/negative).
+- range_query(l, r): return sum(A[l..r]).
+
+TODO: CP 4.1.
+
 */
 
 #include <iostream>
@@ -73,10 +102,9 @@ public:
       update(b+1, _A[b]);
   }
 
-  void update(int b, int newv) {
-    int delta = -query(b, b) + newv;
+  void update(int b, int v) {
     for(; b <= n; b += lsone(b))
-      ft[b] += delta;
+      ft[b] += v;
   }
 
   int query(int b) {
@@ -100,7 +128,7 @@ int main() {
   cout << ft.query(3) << '\n';  // 6
 
   ft.update(2, 10);
-  cout << ft.query(2, 3) << '\n';  // 13
+  cout << ft.query(2, 3) << '\n';
  
   return 0;
 }
